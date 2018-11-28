c0bb6d472b5112b141f09e82e8e0e445b6486644
Franklin Tarter <franklintarter@gmail.com>
2018-11-28 12:07:40 -0800

    EF wired up, first migration

--- AspNetCore.CQRS.Data/AspNetCore.CQRS.Data/DataContext.cs
+++ AspNetCore.CQRS.Data/AspNetCore.CQRS.Data/DataContext.cs
@@ -1,8 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AspNetCore.CQRS.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCore.CQRS.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AspNetCoreCQRS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+
+            optionsBuilder.EnableSensitiveDataLogging();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Person> People { get; set; }
     }
 }
---
+++ AspNetCore.CQRS.Data/AspNetCore.CQRS.Data/Migrations/20181128200601_Initial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCore.CQRS.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastModifiedAt = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
