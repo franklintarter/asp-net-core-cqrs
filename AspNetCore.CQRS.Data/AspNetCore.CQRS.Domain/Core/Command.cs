b1563a7d2bae0f3c231c4fe29d2210cb34be0883
Franklin Tarter <franklintarter@gmail.com>
2018-11-28 17:41:37 -0800

    initialize CQRS boilerplate and 3rd party libs

---
+++ AspNetCore.CQRS.Data/AspNetCore.CQRS.Domain/Core/Command.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.CQRS.Domain.Core
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+
+        public abstract bool IsValid();
+    }
+}
